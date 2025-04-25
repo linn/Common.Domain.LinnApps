0eecf2d438d8e29b3c08fdc4fa3a282ccbd3ce23
Lewis Renfrew <lewis.renfrew@linn.co.uk>
2025-04-25 14:23:40 +0100

    first commit

---
+++ Scripts/publish.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+set -v
+dotnet pack -c release
+dotnet nuget push "$package" --api-key $NUGET_API_KEY --source https://www.nuget.org/api/v2/package --skip-duplicate
+
+if [ "${TRAVIS_BRANCH}" = "main" ]; then
+  if [ "${TRAVIS_PULL_REQUEST}" = "false" ]; then
+
+    echo Starting publish...
+
+    # generate the nuget files
+    dotnet pack -c release
+
+    # publish packages to nuget
+    for package in ./src/**/bin/release/*.nupkg; do
+      dotnet nuget push "$package" --api-key $NUGET_API_KEY --source https://www.nuget.org/api/v2/package --skip-duplicate
+    done
+
+    echo ...done publishing
+  else
+    echo Skipping publish
+  fi
+else
+  echo Skipping publish
+fi
---
+++ Services/ILabelPrinter.cs
@@ -0,0 +1,13 @@
+﻿namespace Linn.Common.Domain.LinnApps.Services;
+
+using System.Threading.Tasks;
+
+public interface ILabelPrinter
+{
+    Task<(bool Success, string Message)> PrintLabelsAsync(
+        string fileName,
+        string printer,
+        int qty,
+        string template,
+        string data);
+}
